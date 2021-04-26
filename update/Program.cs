e632c7a5676499bf0be2cd848d699c8380c5611c
Lucas Frigo de Souza <lucas.iteris@easynvest.com.br>
2021-04-26 14:18:15 -0300

    Altera layout do perfil

--- update/Program.cs
+++ update/Program.cs
@@ -6,11 +6,15 @@ using System.Text.Json;
 
 var template = @"# ✌ Olá, seja bem-vindo(a)!
 
-Me chamo **Lucas**, sou formado em Engenharia da Computação, tenho MBA em Engenharia de Software e estou cursando um MBA em CyberSecurity e Ethical Hacking.
-Com 10 anos de experiência em TI, trabalho atualmente como desenvolvedor fullstack, principalmente com C# e ReactJS.
+Me chamo **Lucas**, sou formado em Engenharia da Computação, tenho MBA em Engenharia de Software e estou cursando um MBA em CyberSecurity.
+Com 10 anos de experiência em TI, trabalho atualmente como desenvolvedor sênior na [Iteris Consultoria e Software](https://www.iteris.com.br/).
 
-Busco sempre novos conhecimentos nos mais diversos assuntos e tecnologias, e nos meus repositórios você vai encontrar todos tipos de projetos.
-</br>
+Meu foco é desenvolvimento backend com .NET e em DevSec, mas sempre busco novos conhecimentos nos mais diversos assuntos e tecnologias, e nos meus repositórios você vai encontrar todos tipos de projetos.
+</br><p align=""center"">
+<a href=""https://www.linkedin.com/in/lfrigodesouza/""><img src=""https://img.shields.io/badge/-LinkedIn-0077B5?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/lfrigodesouza/""></a>
+<a href=""https://twitter.com/lfrigodesouza/""><img src=""https://img.shields.io/badge/-Twitter-1DA1F2?style=flat-square&logo=twitter&logoColor=white&link=https://twitter.com/lfrigodesouza/""></a>
+<a href=""https://LFrigoDeSouza.NET/""><img src=""https://img.shields.io/badge/-LFS.NET-9e9e9e?style=flat-square&logo=microsoft-edge&logoColor=white&link=https://LFrigoDeSouza.NET/""></a>
+</p>
 
 ## ✒️Artigos Recentes
 <ul>
@@ -18,21 +22,10 @@ Busco sempre novos conhecimentos nos mais diversos assuntos e tecnologias, e nos
 <li style=""list-style-type: none;""><a href=""https://blog.lfrigodesouza.net"" target=""_blank"">Veja mais...</a></li>
 </ul>
 
-## 📞 Onde me encontrar
+## 👨‍💻 Meu GitHub
 <p align=""center"">
-<a href=""https://www.linkedin.com/in/lfrigodesouza/""><img src=""https://img.shields.io/badge/-LinkedIn-0077B5?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/lfrigodesouza/""></a>
-<a href=""https://twitter.com/lfrigodesouza/""><img src=""https://img.shields.io/badge/-Twitter-1DA1F2?style=flat-square&logo=twitter&logoColor=white&link=https://twitter.com/lfrigodesouza/""></a>
-<a href=""https://LFrigoDeSouza.NET/""><img src=""https://img.shields.io/badge/-LFS.NET-9e9e9e?style=flat-square&logo=microsoft-edge&logoColor=white&link=https://LFrigoDeSouza.NET/""></a>
-</p>
-</br>
-
-## 👨‍💻 Estatísticas do meu GitHub
-<p align=""center"">
-
 <img src=""https://github-readme-stats.vercel.app/api/top-langs/?username=lfrigodesouza&layout=compact&theme=dark""/>
-
 <img src=""https://github-readme-stats.vercel.app/api?username=lfrigodesouza&show_icons=true&theme=dark"">
-
 </p>
 ";
 
